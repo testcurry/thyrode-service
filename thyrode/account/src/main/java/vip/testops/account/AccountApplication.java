2b2487f0edf0bb86ba6b7ee5b6cf711d79f35a14
TestCurry <987899472@qq.com>
2021-02-15 04:46:16 +0800

    add account servcie

--- thyrode/account/src/main/java/vip/testops/account/AccountApplication.java
+++ thyrode/account/src/main/java/vip/testops/account/AccountApplication.java
@@ -1,33 +1,22 @@
 package vip.testops.account;
 
-<<<<<<< HEAD
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
 
-@SpringBootApplication
-@EnableEurekaClient
-=======
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
->>>>>>> origin/main
 public class AccountApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
     }
 
-<<<<<<< HEAD
     @Bean
     public DataSource dataSource(){
         return DruidDataSourceBuilder.create().build();
     }
-=======
->>>>>>> origin/main
 }
