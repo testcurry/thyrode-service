a7e112178de3d4de3c47d21a84540c07772b82f1
TestCurry <987899472@qq.com>
2021-02-18 00:48:57 +0800

    add manager service

--- thyrode/manager/src/main/java/vip/testops/manager/ManagerApplication.java
+++ thyrode/manager/src/main/java/vip/testops/manager/ManagerApplication.java
@@ -1,13 +1,13 @@
-package vip.testops.account;
+package vip.testops.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccountApplication {
+public class ManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AccountApplication.class, args);
+        SpringApplication.run(ManagerApplication.class, args);
     }
 
 }
